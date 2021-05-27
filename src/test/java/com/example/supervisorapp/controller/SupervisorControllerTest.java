61bc95e5bc8cfff249614a9fc5a0eac0b94a7d8f
Jaron Ewell <jaronewell@outlook.com>
2021-05-26 23:47:57 -0400

    Added tests for validation on submit endpoint

--- src/test/java/com/example/supervisorapp/controller/SupervisorControllerTest.java
+++ src/test/java/com/example/supervisorapp/controller/SupervisorControllerTest.java
@@ -1,12 +1,19 @@
 package com.example.supervisorapp.controller;
 
 import com.example.supervisorapp.service.SupervisorService;
+import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.Arrays;
 import java.util.List;
@@ -14,11 +21,15 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
+@WebMvcTest
+@AutoConfigureMockMvc
 public class SupervisorControllerTest {
 
-  @InjectMocks private SupervisorController supervisorController;
+  @Autowired private SupervisorController supervisorController;
 
-  @Mock private SupervisorService supervisorService;
+  @MockBean private SupervisorService supervisorService;
+
+  @Autowired private MockMvc mockMvc;
 
   @Test
   void getSupervisorsCallsServiceAndReturnsListOfStrings() {
@@ -36,5 +47,62 @@ public class SupervisorControllerTest {
 
     Mockito.verify(supervisorService).getAndFormatSupervisors();
     assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  void submitRegistrationReturnsOkWhenSuccessful() throws Exception {
+
+    String requestString =
+        "{\n"
+            + "    \"firstName\": \"Tom\",\n"
+            + "    \"lastName\": \"Gregory\",\n"
+            + "    \"email\": \"tomgregory@gmail.com\",\n"
+            + "    \"phoneNumber\": \"123-456-7890\",\n"
+            + "    \"supervisor\": \n"
+            + "    {\n"
+            + "        \"id\": \"5\",\n"
+            + "        \"phone\": \"(964) 512-1445\",\n"
+            + "        \"jurisdiction\": \"8\",\n"
+            + "        \"identificationNumber\": \"93fd04ee-655a-47c2-832b-d83056d29e1e\",\n"
+            + "        \"firstName\": \"Kieran\",\n"
+            + "        \"lastName\": \"Torphy\"\n"
+            + "    }\n"
+            + "}";
+
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.post("/api/submit")
+                .content(requestString)
+                .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isOk());
+  }
+
+  @Test
+  void submitRegistrationReturnsBadRequestWhenValidationFails() throws Exception {
+
+    String requestString =
+        "{\n"
+            + "    \"firstName\": \"Tom\",\n"
+            + "    \"email\": \"tomgregory@gmail.com\",\n"
+            + "    \"phoneNumber\": \"123-456-7890\",\n"
+            + "    \"supervisor\": \n"
+            + "    {\n"
+            + "        \"id\": \"5\",\n"
+            + "        \"phone\": \"(964) 512-1445\",\n"
+            + "        \"jurisdiction\": \"8\",\n"
+            + "        \"identificationNumber\": \"93fd04ee-655a-47c2-832b-d83056d29e1e\",\n"
+            + "        \"firstName\": \"Kieran\",\n"
+            + "        \"lastName\": \"Torphy\"\n"
+            + "    }\n"
+            + "}";
+
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.post("/api/submit")
+                .content(requestString)
+                .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isBadRequest())
+        .andExpect(
+            MockMvcResultMatchers.jsonPath("$.lastName", Is.is("Last Name must be provided")));
   }
 }
